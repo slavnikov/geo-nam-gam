add932be42acde4cfb967bb493e3748fe3c408d5
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2024-02-19 20:28:41 -0500

    Add some signature specifities to the cookie middleware;

--- src/cookie-mandater/cookie-mandater.middleware.ts
+++ src/cookie-mandater/cookie-mandater.middleware.ts
@@ -4,14 +4,14 @@ import { v4 } from 'uuid';
 
 @Injectable()
 export class CookieMandaterMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => void) {
+  use(req: Request, res: Response, next: () => void): void {
     const cookieId: string = this.getCookieId(req);
 
     res.cookie('cookie_id', cookieId, {signed: true})
     next();
   }
 
-  getCookieId(req: Request): string {
+  private getCookieId(req: Request): string {
     const cookieId: string = req.signedCookies['cookie_id'];
 
     return cookieId || v4();
