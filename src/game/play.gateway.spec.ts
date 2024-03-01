6f071bf9c17a4ba0b47157fbc19393b15453aa81
Dmitry Slavnikov <dmitry@slavnikov.com>
2024-02-29 21:25:54 -0500

    Implement tests for basic socket connection behavior;

--- src/game/play.gateway.spec.ts
+++ src/game/play.gateway.spec.ts
@@ -51,4 +51,53 @@ describe('PlayGateway', () => {
 
     expect(resWaiter).toBeTruthy();
   });
+
+  it('should close a socket opened without a cookie', async() => {
+    const resWaiter: boolean = await new Promise<boolean>((resolve, _) => {
+      const clientSocket = new WebSocket('http://127.0.1.1:3000');
+
+      clientSocket.on('close', (code:number) => {
+        if(code === 1011)
+          resolve(true);
+      });
+      setTimeout(() => resolve(false), 1000)
+    });
+
+    expect(resWaiter).toBeTruthy();
+  });
+
+  it('should close a socket opened with a tampered cookie', async() => {
+    const resWaiter: boolean = await new Promise<boolean>((resolve, _) => {
+      const rawCookieVal = 's%3A8c48fcfa-065e-4955-a61a-e43613015f12.B1qK%2BSYNy%2F83y0GT3J1cJvu09fwp63aXLsI%2FUk0z2G8';
+      const clientSocket = new WebSocket('http://127.0.1.1:3000', {headers: {cookie: `cookie_id=${rawCookieVal};`}});
+
+      clientSocket.on('close', (code:number) => {
+        if(code === 1011)
+          resolve(true);
+      });
+      setTimeout(() => resolve(false), 1000)
+    });
+
+    expect(resWaiter).toBeTruthy();
+  });
+
+  it('should stay open if connected with a valid cookie', async() => {
+    const playerCacheSetter = jest.spyOn(gateway.playerCache, 'set');
+    const resWaiter: boolean = await new Promise<boolean>((resolve, _) => {
+      const rawCookieVal = 's%3A8c48fcfa-065e-4955-a61a-e43613015f12.A1qK%2BSYNy%2F83y0GT3J1cJvu09fwp63aXLsI%2FUk0z2G8';
+      const clientSocket = new WebSocket('http://127.0.1.1:3000', {headers: {cookie: `cookie_id=${rawCookieVal};`}});
+      let stayedOpen: boolean = false;
+
+      clientSocket.on('close', () => {
+        resolve(stayedOpen === true);
+      });
+      setTimeout(() => {
+        stayedOpen = true;
+        clientSocket.close();
+      }, 1000)
+    });
+
+    expect(resWaiter).toBeTruthy();
+    expect(playerCacheSetter).toHaveBeenCalledTimes(1);
+  });
 });
